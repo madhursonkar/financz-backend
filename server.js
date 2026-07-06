9f9316b63563c6bbbc540db88310c087059dacea
Madhur Sonkar <138878209+madhursonkar@users.noreply.github.com>
2026-07-06 11:31:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static("public"));
